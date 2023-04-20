ffc92f5c7cb01dd44385e33e00f77d93b7acc732
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-20 10:53:38 +0500

    Update raspap.php

--- raspap.php
+++ raspap.php
@@ -1,19 +1,15 @@
-<?php
-
-function getConfig()
+function getConfig(): array
 {
-    $config = array(
+    $config = [
         'admin_user' => 'admin',
         'admin_pass' => '$2y$10$YKIyWAmnQLtiJAy6QgHQ.eCpY4m.HCEbiHaTgN6.acNC6bDElzt.i'
-    );
+    ];
 
-    if (file_exists(RASPI_CONFIG . '/raspap.auth')) {
-        if ($auth_details = fopen(RASPI_CONFIG . '/raspap.auth', 'r')) {
-            $config['admin_user'] = trim(fgets($auth_details));
-            $config['admin_pass'] = trim(fgets($auth_details));
-            fclose($auth_details);
-        }
+    $authFile = RASPI_CONFIG . '/raspap.auth';
+    if (file_exists($authFile) && $authDetails = file($authFile, FILE_IGNORE_NEW_LINES)) {
+        $config['admin_user'] = $authDetails[0];
+        $config['admin_pass'] = $authDetails[1];
     }
+
     return $config;
 }
-
