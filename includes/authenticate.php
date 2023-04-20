1a30f60686f41702b77ed7bbc91b24001a868dca
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-20 11:23:52 +0500

    Update authenticate.php

--- includes/authenticate.php
+++ includes/authenticate.php
@@ -1,20 +1,24 @@
 <?php
-$user = $_SERVER['PHP_AUTH_USER'] ?? "";
-$pass = $_SERVER['PHP_AUTH_PW'] ?? "";
+// Get the HTTP Basic Authentication credentials from the request.
+$username = $_SERVER['PHP_AUTH_USER'] ?? '';
+$password = $_SERVER['PHP_AUTH_PW'] ?? '';
 
-require_once RASPI_CONFIG.'/raspap.php';
+require_once RASPI_CONFIG . '/raspap.php';
 $config = getConfig();
 
-$validated = ($user == $config['admin_user']) && password_verify($pass, $config['admin_pass']);
+// Validate the credentials against the configured admin username and password.
+$is_authenticated = ($username === $config['admin_user']) && password_verify($password, $config['admin_pass']);
 
-if (!$validated) {
+if (!$is_authenticated) {
+    // If the credentials are invalid, send a 401 Unauthorized response and exit.
     header('WWW-Authenticate: Basic realm="RaspAP"');
     if (function_exists('http_response_code')) {
-        // http_response_code will respond with proper HTTP version back.
         http_response_code(401);
     } else {
         header('HTTP/1.0 401 Unauthorized');
     }
-
-    exit('Not authorized'.PHP_EOL);
+    echo 'Not authorized' . PHP_EOL;
+} else {
+    // If the credentials are valid, allow the user to access the protected resource.
+    // ...
 }
