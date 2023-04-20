1771ce6105a7b5b8fd6266f64dfacdc862f8fab2
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-20 11:25:57 +0500

    create instance for db update & fix logic

--- ajax/adblock/update_blocklist.php
+++ ajax/adblock/update_blocklist.php
@@ -4,7 +4,7 @@ require '../../includes/csrf.php';
 require_once '../../includes/config.php';
 
 if (isset($_POST['blocklist_id'])) {
-    $blocklist_id = $_POST['blocklist_id'];
+    $blocklist_id = htmlspecialchars($_POST['blocklist_id']);
     $notracking_url = "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/";
 
     switch ($blocklist_id) {
@@ -14,11 +14,15 @@ if (isset($_POST['blocklist_id'])) {
         case "notracking-domains":
             $file = "domains.txt";
             break;
+        default:
+            exit('Invalid blocklist ID');
     }
     $blocklist = $notracking_url . $file;
 
-    exec("sudo /etc/raspap/adblock/update_blocklist.sh $blocklist $file " .RASPI_ADBLOCK_LISTPATH, $return);
+    $stmt = $pdo->prepare("UPDATE blocklists SET url = ?, file = ? WHERE id = 1");
+    $stmt->execute([$blocklist, $file]);
+
+    exec("sudo /etc/raspap/adblock/update_blocklist.sh $blocklist $file " . RASPI_ADBLOCK_LISTPATH, $return);
     $jsonData = ['return'=>$return];
     echo json_encode($jsonData);
 }
-
