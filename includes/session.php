db0109c3e815f4b409808a3ea375ff45fa64d205
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-20 13:18:51 +0500

    create session handling

--- includes/session.php
+++ includes/session.php
@@ -1,5 +1,19 @@
 <?php
 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    try {
+        // Set session options if needed
+        // session_set_cookie_params(3600, '/', '', false, true);
+        // session_cache_limiter('private_no_expire');
+        // session_save_path('/custom/session/path');
+       // Check if the session ID is valid
+        if (session_id() === '') {
+            throw new Exception('Invalid session ID');
+        }
+
+        session_start();
+    } catch (Exception $e) {
+        // Display an error message to the user
+        echo 'Error starting session: ' . $e->getMessage();
+    }
 }
