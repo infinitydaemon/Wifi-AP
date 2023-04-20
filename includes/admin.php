4d4b9f0cad266df8ed7b73412ee5adb9df491547
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-20 10:56:30 +0500

    Update admin.php

--- includes/admin.php
+++ includes/admin.php
@@ -2,26 +2,41 @@
 
 require_once 'includes/status_messages.php';
 
+if (!defined('RASPI_ADMIN_DETAILS')) {
+    define('RASPI_ADMIN_DETAILS', '/path/to/admin/details');
+}
+
+/**
+ * Displays the authentication configuration form and updates the admin password if submitted.
+ *
+ * @param string $username The current username.
+ * @param string $password The current password.
+ */
 function DisplayAuthConfig($username, $password)
 {
+    // Initialize a status message container
     $status = new StatusMessages();
+
+    // If the form has been submitted
     if (isset($_POST['UpdateAdminPassword'])) {
+        // If the old password matches
         if (password_verify($_POST['oldpass'], $password)) {
-            $new_username=trim($_POST['username']);
+            $new_username = trim($_POST['username']);
+            // If the new passwords don't match
             if ($_POST['newpass'] !== $_POST['newpassagain']) {
                 $status->addMessage('New passwords do not match', 'danger');
-            } elseif ($new_username == '') {
+            // If the new username is empty
+            } elseif ($new_username === '') {
                 $status->addMessage('Username must not be empty', 'danger');
             } else {
+                // Create the admin details file if it doesn't exist
                 if (!file_exists(RASPI_ADMIN_DETAILS)) {
-                    $tmpauth = fopen(RASPI_ADMIN_DETAILS, 'w');
-                    fclose($tmpauth);
+                    file_put_contents(RASPI_ADMIN_DETAILS, '');
                 }
 
-                if ($auth_file = fopen(RASPI_ADMIN_DETAILS, 'w')) {
-                    fwrite($auth_file, $new_username.PHP_EOL);
-                    fwrite($auth_file, password_hash($_POST['newpass'], PASSWORD_BCRYPT).PHP_EOL);
-                    fclose($auth_file);
+                // Write the new username and password hash to the admin details file
+                if (file_put_contents(RASPI_ADMIN_DETAILS, $new_username.PHP_EOL, LOCK_EX | FILE_APPEND) !== false &&
+                    file_put_contents(RASPI_ADMIN_DETAILS, password_hash($_POST['newpass'], PASSWORD_BCRYPT).PHP_EOL, LOCK_EX | FILE_APPEND) !== false) {
                     $username = $new_username;
                     $status->addMessage('Admin password updated');
                 } else {
@@ -33,5 +48,6 @@ function DisplayAuthConfig($username, $password)
         }
     }
 
-    echo renderTemplate("admin", compact("status", "username"));
+    // Render the authentication configuration template
+    echo renderTemplate('admin', compact('status', 'username'));
 }
