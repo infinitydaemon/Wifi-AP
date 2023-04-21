0382d394f48b31572c0f42c3b3c39f1d2a1a4e7e
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-21 14:55:40 +0500

    Update del_ovpncfg.php
    * Added a regular expression to validate the input for $_POST['cfg_id'] and remove any characters that are not alphanumeric or underscore.
    * Escaped the shell argument passed to exec() function to prevent command injection attacks.

--- ajax/openvpn/del_ovpncfg.php
+++ ajax/openvpn/del_ovpncfg.php
@@ -4,9 +4,9 @@ require_once '../../includes/config.php';
 require_once '../../includes/functions.php';
 
 if (isset($_POST['cfg_id'])) {
-    $ovpncfg_id = $_POST['cfg_id'];
+    $ovpncfg_id = preg_replace('/[^a-zA-Z0-9_]/', '', $_POST['cfg_id']);
     $ovpncfg_files = pathinfo(RASPI_OPENVPN_CLIENT_LOGIN, PATHINFO_DIRNAME).'/'.$ovpncfg_id.'_*.conf';
-    exec("sudo rm $ovpncfg_files", $return);
+    exec("sudo rm ".escapeshellarg($ovpncfg_files), $return);
     $jsonData = ['return'=>$return];
     echo json_encode($jsonData);
 }
