8a46b42c9bbc07eeec32ba791b8f599dd65d5c7f
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-21 14:50:14 +0500

    Update get_wgkey.php

--- ajax/networking/get_wgkey.php
+++ ajax/networking/get_wgkey.php
@@ -1,20 +1,20 @@
 <?php
 
-require '../../includes/csrf.php';
+require_once '../../includes/csrf.php';
 require_once '../../includes/config.php';
 
 $entity = $_POST['entity'];
 
-if (isset($entity)) {
+if (!empty($entity)) {
    
     // generate public/private key pairs for entity
-    $pubkey = RASPI_WIREGUARD_PATH.$entity.'-public.key';
-    $privkey = RASPI_WIREGUARD_PATH.$entity.'-private.key';
-    $pubkey_tmp = '/tmp/'.$entity.'-public.key';
-    $privkey_tmp = '/tmp/'.$entity.'-private.key';
+    $pubkey = RASPI_WIREGUARD_PATH . $entity . '-public.key';
+    $privkey = RASPI_WIREGUARD_PATH . $entity . '-private.key';
+    $pubkey_tmp = '/tmp/' . $entity . '-public.key';
+    $privkey_tmp = '/tmp/' . $entity . '-private.key';
      
     exec("sudo wg genkey | tee $privkey_tmp | wg pubkey > $pubkey_tmp", $return);
-    $wgdata['pubkey'] = str_replace("\n",'',file_get_contents($pubkey_tmp));
+    $wgdata['pubkey'] = str_replace("\n", '', file_get_contents($pubkey_tmp));
     exec("sudo mv $privkey_tmp $privkey", $return);
     exec("sudo mv $pubkey_tmp $pubkey", $return);
 
