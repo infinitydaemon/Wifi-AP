bf6f2c57c7ada6906af78b49da1899018d837d5e
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-21 14:36:21 +0500

    Update update_blocklist.php
    * Add bit more security

--- ajax/adblock/update_blocklist.php
+++ ajax/adblock/update_blocklist.php
@@ -4,7 +4,15 @@ require '../../includes/csrf.php';
 require_once '../../includes/config.php';
 
 if (isset($_POST['blocklist_id'])) {
-    $blocklist_id = htmlspecialchars($_POST['blocklist_id']);
+    $blocklist_id = $_POST['blocklist_id'];
+
+    // Sanitize the input
+    if (!in_array($blocklist_id, ["notracking-hostnames", "notracking-domains"])) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid blocklist ID']);
+        exit;
+    }
+
     $notracking_url = "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/";
 
     switch ($blocklist_id) {
@@ -14,15 +22,17 @@ if (isset($_POST['blocklist_id'])) {
         case "notracking-domains":
             $file = "domains.txt";
             break;
-        default:
-            exit('Invalid blocklist ID');
     }
     $blocklist = $notracking_url . $file;
 
+    // Use prepared statements to update the database
     $stmt = $pdo->prepare("UPDATE blocklists SET url = ?, file = ? WHERE id = 1");
     $stmt->execute([$blocklist, $file]);
 
-    exec("sudo /etc/raspap/adblock/update_blocklist.sh $blocklist $file " . RASPI_ADBLOCK_LISTPATH, $return);
-    $jsonData = ['return'=>$return];
-    echo json_encode($jsonData);
+    // Run the shell command to update the blocklist
+    exec("sudo /etc/raspap/adblock/update_blocklist.sh $blocklist $file " . RASPI_ADBLOCK_LISTPATH, $output, $return);
+
+    // Return a JSON response with the output and return code
+    $response = ['output' => $output, 'return' => $return];
+    echo json_encode($response);
 }
