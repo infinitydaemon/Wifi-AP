2f0694c280e9fbf6693735cc555b9e093fc15f18
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-21 14:42:18 +0500

    Update clearlog.php

--- ajax/logging/clearlog.php
+++ ajax/logging/clearlog.php
@@ -3,10 +3,19 @@
 require_once '../../includes/config.php';
 require_once '../../includes/functions.php';
 
-if (isset($_POST['logfile'])) {
-    $logfile = $_POST['logfile'];
+define('SUDO_CMD', '/usr/bin/sudo');
 
-    // truncate requested log file
-    exec("sudo truncate -s 0 $logfile", $return);
+if (isset($_POST['logfile'])) {
+    $logfile = realpath($_POST['logfile']);
+    $basename = basename($logfile);
+
+    // Validate that the file path is not empty and is inside the logs directory
+    if (!$logfile || strpos($logfile, LOGS_DIR) !== 0) {
+        http_response_code(400);
+        exit('Invalid file path');
+    }
+
+    // truncate requested log file using sudo
+    exec(SUDO_CMD . " truncate -s 0 $basename", $return);
     echo json_encode($return);
 }
