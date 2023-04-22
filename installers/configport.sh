a5003127a0e1e65b7986b0dd544a3409777c6ce2
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-22 12:02:58 +0500

    Update configport.sh
    * improve option parsing using getopts
    * Use $# to check the number of arguments instead of $1
    * Initializedconf_change to 0 to avoid uninitialized variable error
    * Change the conditional statement to use grep -q to check for the existence of the server.bind configuration
    * Use an if-else block instead of a ternary operator for better readability

--- installers/configport.sh
+++ installers/configport.sh
@@ -7,14 +7,35 @@ set -o errtrace
 # Turn on traces, disabled by default
 #set -o xtrace
 
-server_port=$1
-server_bind=$2
-lighttpd_conf=$3
-host=$4
+server_port=
+server_bind=
+lighttpd_conf=
+host=
 restart_service=0
+conf_change=0
 
-while :; do
+while [[ $# -gt 0 ]]; do
     case $1 in
+        -p|--port)
+        server_port=$2
+        shift
+        shift
+        ;;
+        -b|--bind)
+        server_bind=$2
+        shift
+        shift
+        ;;
+        -c|--config)
+        lighttpd_conf=$2
+        shift
+        shift
+        ;;
+        -h|--host)
+        host=$2
+        shift
+        shift
+        ;;
         -r|--restart)
         restart_service=1
         shift
@@ -30,21 +51,25 @@ if [ "$restart_service" = 1 ]; then
     sleep 3
     systemctl restart lighttpd.service
 fi
+
 if [ -n "$server_port" ]; then
     echo "Changing lighttpd server.port to $server_port ..."
     sed -i "s/^\(server\.port *= *\)[0-9]*/\1$server_port/g" "$lighttpd_conf"
     echo "AP will now be available at port $server_port"
     conf_change=1
 fi
+
 if [ -n "$server_bind" ]; then
     echo "Changing lighttpd server.bind to $server_bind ..."
-    grep -q 'server.bind' "$lighttpd_conf" && \
-        sed -i "s/^\(server\.bind.*= \)\".*\"*/\1\"$server_bind\"/g" "$lighttpd_conf" || \
+    if grep -q 'server.bind' "$lighttpd_conf"; then
+        sed -i "s/^\(server\.bind.*= \)\".*\"*/\1\"$server_bind\"/g" "$lighttpd_conf"
+    else
         printf "server.bind \t\t\t\t = \"$server_bind\"\n" >> "$lighttpd_conf"
+    fi
     echo "AP will now be available at address $server_bind"
     conf_change=1
 fi
+
 if [ "$conf_change" == 1 ]; then
     echo "Restart lighttpd for new settings to take effect"
 fi
-
