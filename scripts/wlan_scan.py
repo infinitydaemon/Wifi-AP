e265fcc9a225cc18c760449b54c971d9f7aa9529
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-05-21 11:51:40 +0500

    Update wlan_scan.py

--- scripts/wlan_scan.py
+++ scripts/wlan_scan.py
@@ -1,16 +1,34 @@
-import subprocess
+# pip install pywifi matplotlib
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import pywifi
 
 def scan_wireless_channels(interface):
-    try:
-        output = subprocess.check_output(["iwlist", interface, "scan"], stderr=subprocess.STDOUT)
-        output = output.decode("utf-8")
-        return output
-    except subprocess.CalledProcessError as e:
-        print(f"Error scanning wireless channels: {e.output.decode('utf-8')}")
-        return None
+    wifi = pywifi.PyWiFi()
+    iface = wifi.interfaces()[interface]
+    iface.scan()
+    time.sleep(5)  # Wait for scan completion.
 
-interface = "wlan0"
-scan_results = scan_wireless_channels(interface)
+    networks = iface.scan_results()
+    channels = set(network.channel for network in networks)
 
-if scan_results:
-    print(scan_results)
+    return channels
+
+def generate_graph(channels):
+    channel_counts = [0] * 14  # Use 2.4Ghz bands
+    for channel in channels:
+        channel_counts[channel] += 1
+
+    plt.bar(range(1, 15), channel_counts)
+    plt.xlabel('Channel')
+    plt.ylabel('Count')
+    plt.title('Wireless Channel Distribution')
+    plt.xticks(np.arange(1, 15))
+    plt.show()
+
+if __name__ == '__main__':
+    wlan_interface = 0  # Default interface on the board
+
+    channels = scan_wireless_channels(wlan_interface)
+    generate_graph(channels)
